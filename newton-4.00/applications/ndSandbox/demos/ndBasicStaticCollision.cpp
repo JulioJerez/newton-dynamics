4fa90d7757700b681cadf643234d68246ca98546
NewtonDynamics <jerezjulio0@gmail.com>
2026-01-22 11:28:35 -0800

    re creating leadwork test scene in
    good knew is that the bug does not happens in 4.xx

--- newton-4.00/applications/ndSandbox/demos/ndBasicStaticCollision.cpp
+++ newton-4.00/applications/ndSandbox/demos/ndBasicStaticCollision.cpp
@@ -17,38 +17,6 @@
 #include "ndDemoEntityNotify.h"
 #include "ndDemoEntityManager.h"
 
-void ndBasicStaticMeshCollision (ndDemoEntityManager* const scene)
-{
-	ndSharedPtr<ndBody> background(BuildPlayground(scene));
-	
-	class PlaceMatrix : public ndMatrix
-	{
-		public:
-		PlaceMatrix(ndFloat32 x, ndFloat32 y, ndFloat32 z)
-			:ndMatrix(ndGetIdentityMatrix())
-		{
-			m_posit.m_x = x;
-			m_posit.m_y = y;
-			m_posit.m_z = z;
-		}
-	};
-	
-	AddBox(scene, PlaceMatrix(10.0f, 1.0f, 0.0f), 30.0f, 2.0f, 0.25f, 2.5f);
-	AddBox(scene, PlaceMatrix(12.0f, 1.5f, 1.5f), 30.0f, 2.0f, 0.25f, 2.5f);
-	AddBox(scene, PlaceMatrix(13.0f, 1.0f, -1.0f), 30.0f, 1.0f, 0.25f, 1.0f);
-	AddConvexHull(scene, PlaceMatrix(12.0f, 1.0f, 3.0f), 40.0f, 0.6f, 1.0f, 15);
-	AddConvexHull(scene, PlaceMatrix(12.0f, 1.0f, 1.0f), 40.0f, 0.7f, 1.0f, 10);
-	AddConvexHull(scene, PlaceMatrix(13.0f, 1.0f, 2.0f), 40.0f, 0.5f, 1.2f, 6);
-	AddConvexHull(scene, PlaceMatrix(14.0f, 1.0f, 4.0f), 40.0f, 0.6f, 1.0f, 15);
-	AddConvexHull(scene, PlaceMatrix(14.0f, 1.0f, -1.0f), 40.0f, 0.7f, 1.0f, 10);
-	AddConvexHull(scene, PlaceMatrix(13.0f, 1.0f, -2.0f), 40.0f, 0.5f, 1.2f, 6);
-	//AddCapsulesStacks(scene, PlaceMatrix(45.0f, 0.0f, 0.0f), 10.0f, 0.5f, 0.5f, 1.0f, 5, 8, 7);
-	
-	ndQuaternion rot(ndYawMatrix(30.0f * ndDegreeToRad));
-	ndVector origin(-40.0f, 15.0f, 20.0f, 1.0f);
-	scene->SetCameraMatrix(rot, origin);
-}
-
 void ndBasicSceneCompoundCollision(ndDemoEntityManager* const scene)
 {
 	ndSharedPtr<ndBody> compoundScene(BuildCompoundScene(scene, ndGetIdentityMatrix()));
@@ -80,3 +48,77 @@ void ndBasicSceneCompoundCollision(ndDemoEntityManager* const scene)
 	ndVector origin(-40.0f, 15.0f, 20.0f, 1.0f);
 	scene->SetCameraMatrix(rot, origin);
 }
+
+#if 0
+void ndBasicStaticMeshCollision(ndDemoEntityManager* const scene)
+{
+	ndSharedPtr<ndBody> background(BuildPlayground(scene));
+
+	class PlaceMatrix : public ndMatrix
+	{
+		public:
+		PlaceMatrix(ndFloat32 x, ndFloat32 y, ndFloat32 z)
+			:ndMatrix(ndGetIdentityMatrix())
+		{
+			m_posit.m_x = x;
+			m_posit.m_y = y;
+			m_posit.m_z = z;
+		}
+	};
+
+	AddBox(scene, PlaceMatrix(10.0f, 1.0f, 0.0f), 30.0f, 2.0f, 0.25f, 2.5f);
+	AddBox(scene, PlaceMatrix(12.0f, 1.5f, 1.5f), 30.0f, 2.0f, 0.25f, 2.5f);
+	AddBox(scene, PlaceMatrix(13.0f, 1.0f, -1.0f), 30.0f, 1.0f, 0.25f, 1.0f);
+	AddConvexHull(scene, PlaceMatrix(12.0f, 1.0f, 3.0f), 40.0f, 0.6f, 1.0f, 15);
+	AddConvexHull(scene, PlaceMatrix(12.0f, 1.0f, 1.0f), 40.0f, 0.7f, 1.0f, 10);
+	AddConvexHull(scene, PlaceMatrix(13.0f, 1.0f, 2.0f), 40.0f, 0.5f, 1.2f, 6);
+	AddConvexHull(scene, PlaceMatrix(14.0f, 1.0f, 4.0f), 40.0f, 0.6f, 1.0f, 15);
+	AddConvexHull(scene, PlaceMatrix(14.0f, 1.0f, -1.0f), 40.0f, 0.7f, 1.0f, 10);
+	AddConvexHull(scene, PlaceMatrix(13.0f, 1.0f, -2.0f), 40.0f, 0.5f, 1.2f, 6);
+	//AddCapsulesStacks(scene, PlaceMatrix(45.0f, 0.0f, 0.0f), 10.0f, 0.5f, 0.5f, 1.0f, 5, 8, 7);
+
+	ndQuaternion rot(ndYawMatrix(30.0f * ndDegreeToRad));
+	ndVector origin(-40.0f, 15.0f, 20.0f, 1.0f);
+	scene->SetCameraMatrix(rot, origin);
+}
+#else
+
+void ndBasicStaticMeshCollision(ndDemoEntityManager* const scene)
+{
+	ndRenderMeshLoader loader(*scene->GetRenderer());
+	loader.LoadMesh(ndGetWorkingFileName("leadwerktestscene.nd"));
+	scene->AddEntity(loader.m_renderMesh);
+
+	ndWorld* const world = scene->GetWorld();
+	ndList<ndSharedPtr<ndMesh>>& children = loader.m_mesh->GetChildren();
+	for (ndList<ndSharedPtr<ndMesh>>::ndNode* child = children.GetFirst(); child; child = child->GetNext())
+	{
+		const ndString& name = child->GetInfo()->GetName();
+		bool test = (name == "Box");
+		test = test || (name == "Cylinder");
+		if (test)
+		{
+			ndSharedPtr<ndShapeInstance> shape (child->GetInfo()->CreateCollisionConvex());
+
+			ndSharedPtr<ndBody> body(new ndBodyDynamic());
+			ndMatrix matrix(child->GetInfo()->CalculateGlobalMatrix());
+			body->SetMatrix(matrix);
+			body->GetAsBodyKinematic()->SetCollisionShape(**shape);
+			world->AddBody(body);
+		}
+	}
+
+	ndMatrix matrix(ndGetIdentityMatrix());
+	matrix.m_posit.m_x = 10.0f;
+	matrix.m_posit.m_z = 4.0f;
+	matrix.m_posit.m_y = 3.0f;
+	//ndSharedPtr<ndBody> testBody(AddCylinder(scene, matrix, ndFloat32(10.0f), 0.7f, 1.8f, 0.7f));
+	ndSharedPtr<ndBody> testBody(AddBox(scene, matrix, ndFloat32(10.0f), 0.7f * 0.5f, 1.8f * 0.5f, 0.7f * 0.5f));
+
+
+	ndMatrix camMatrix(ndGetIdentityMatrix());
+	ndQuaternion rot(camMatrix);
+	ndVector origin(-0.0f, 5.0f, 0.0f, 0.0f);
+	scene->SetCameraMatrix(rot, origin);
+}
+#endif
