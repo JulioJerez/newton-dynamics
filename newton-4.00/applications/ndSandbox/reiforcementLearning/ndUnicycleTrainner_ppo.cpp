a78d6ced83981659f4f874ce9671a30d26df921b
NewtonDynamics <jerezjulio0@gmail.com>
2026-02-12 08:43:58 -0800

    Update ndUnicycleTrainner_ppo.cpp
    training with longer horizon.  0.995 instead of 0.99, that roughly over 200  steps into the future.

--- newton-4.00/applications/ndSandbox/reiforcementLearning/ndUnicycleTrainner_ppo.cpp
+++ newton-4.00/applications/ndSandbox/reiforcementLearning/ndUnicycleTrainner_ppo.cpp
@@ -132,6 +132,7 @@ namespace ndUnicycleTrainer_ppo
 			
 			hyperParameters.m_useGpuBackend = false;
 			hyperParameters.m_batchTrajectoryCount = 1000;
+			hyperParameters.m_discountRewardFactor = 0.995f;
 			hyperParameters.m_hiddenLayersNumberOfNeurons = 64;
 			hyperParameters.m_numberOfActions = m_actionsSize;
 			hyperParameters.m_numberOfObservations = m_observationsSize;
