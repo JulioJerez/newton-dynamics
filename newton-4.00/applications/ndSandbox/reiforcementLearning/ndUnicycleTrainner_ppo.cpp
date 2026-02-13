c15103f9ff5de9bf46acb7e9a933abebb11ecd68
NewtonDynamics <jerezjulio0@gmail.com>
2026-02-13 08:36:14 -0800

    Update ndUnicycleTrainner_ppo.cpp
    try shallow and denser neural net.
    contrary to my assumptions, deeper network seem a lot harder to train
    
    so trying with tow hidden layers but more dense.

--- newton-4.00/applications/ndSandbox/reiforcementLearning/ndUnicycleTrainner_ppo.cpp
+++ newton-4.00/applications/ndSandbox/reiforcementLearning/ndUnicycleTrainner_ppo.cpp
@@ -133,7 +133,8 @@ namespace ndUnicycleTrainer_ppo
 			hyperParameters.m_useGpuBackend = false;
 			hyperParameters.m_batchTrajectoryCount = 1000;
 			hyperParameters.m_discountRewardFactor = 0.995f;
-			hyperParameters.m_hiddenLayersNumberOfNeurons = 64;
+			hyperParameters.m_numberOfHiddenLayers = 2;
+			hyperParameters.m_hiddenLayersNumberOfNeurons = 128;
 			hyperParameters.m_numberOfActions = m_actionsSize;
 			hyperParameters.m_numberOfObservations = m_observationsSize;
 			hyperParameters.m_discountRewardFactor = ndReal(m_discountRewardFactor);
