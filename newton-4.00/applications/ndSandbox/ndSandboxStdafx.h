0c945e0a134dc1ca324dc3e129a32e74fcc325aa
NewtonDynamics <jerezjulio0@gmail.com>
2026-03-14 12:25:06 -0700

    Update ndSandboxStdafx.h

--- newton-4.00/applications/ndSandbox/ndSandboxStdafx.h
+++ newton-4.00/applications/ndSandbox/ndSandboxStdafx.h
@@ -64,11 +64,5 @@
 
 ndString ndGetWorkingFileName(const char* const name);
 
-//// endian conversion
-//ndUnsigned32 SWAP_INT32(ndUnsigned32 x);
-//ndUnsigned16 SWAP_INT16(ndUnsigned16 x);
-//ndUnsigned16 ndIndian16(ndUnsigned16 x);
-//ndUnsigned32 ndIndian32(ndUnsigned32 x);
-
 #endif 
 
