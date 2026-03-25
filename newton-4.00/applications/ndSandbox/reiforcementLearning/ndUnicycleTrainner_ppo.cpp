0aa6716848884e9bb5ac1a38ed6f8961bbf2da86
JulioJerez <jerezjulio0@gmail.com>
2026-03-25 06:18:25 -0700

    Update ndUnicycleTrainner_ppo.cpp

--- newton-4.00/applications/ndSandbox/reiforcementLearning/ndUnicycleTrainner_ppo.cpp
+++ newton-4.00/applications/ndSandbox/reiforcementLearning/ndUnicycleTrainner_ppo.cpp
@@ -103,7 +103,7 @@ namespace ndUnicycleTrainer_ppo
 			,m_outFile(nullptr)
 			,m_timer(ndGetTimeInMicroseconds())
 			,m_savedScore(ndFloat32(-1.0e10f))
-			,m_discountRewardFactor(0.995f)
+			,m_discountRewardFactor(0.99f)
 			,m_horizon(ndFloat32(1.0f) / (ndFloat32(1.0f) - m_discountRewardFactor))
 			,m_lastEpisode(0xfffffff)
 			,m_stopTraining(500 * 1000000)
