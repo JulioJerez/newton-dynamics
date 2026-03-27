fd25cb83447222fb9957283adc13079775645bc2
JulioJerez <jerezjulio0@gmail.com>
2026-03-26 18:11:15 -0700

    Update ndCartpolePlayer.h

--- newton-4.00/applications/ndSandbox/demos/ndCartpolePlayer.h
+++ newton-4.00/applications/ndSandbox/demos/ndCartpolePlayer.h
@@ -19,16 +19,13 @@
 
 namespace ndCarpolePlayer
 {
-	#define CONTROLLER_NAME_SAC		"cartpoleSac"
-	#define CONTROLLER_NAME_PPO		"cartpolePpo"
-
-	#define CART_MASS				ndFloat32(10.0f)
-	#define POLE_MASS				ndFloat32(5.0f)
-
-	#define TRAJECTORY_STEPS		(1024 * 4)
-
+	#define CONTROLLER_NAME_SAC			"cartpoleSac"
+	#define CONTROLLER_NAME_PPO			"cartpolePpo"
+	#define CART_MASS					ndFloat32(10.0f)
+	#define POLE_MASS					ndFloat32(5.0f)
+	#define TRAJECTORY_STEPS			(1024 * 4)
 	#define ND_RANDOM_IMPULSE_MOD		256
-	#define ND_RANDOM_IMPULSE_MAGNITUD	ndFloat32 (2.0f)
+	#define ND_RANDOM_IMPULSE_MAGNITUD	ndFloat32 (5.0f)
 	#define PUSH_ACCEL					ndBrainFloat (-10.0f * DEMO_GRAVITY)
 	#define REWARD_MIN_ANGLE			ndBrainFloat (20.0f * ndDegreeToRad)
 
