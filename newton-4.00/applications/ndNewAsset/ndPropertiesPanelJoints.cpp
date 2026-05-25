6104ff2e28117bdf2c01b54911bc3e08bda45519
NewtonDynamics <jerezjulio0@gmail.com>
2026-05-24 19:39:03 -0700

    Update ndPropertiesPanelJoints.cpp
    more tweaks and bugs

--- newton-4.00/applications/ndNewAsset/ndPropertiesPanelJoints.cpp
+++ newton-4.00/applications/ndNewAsset/ndPropertiesPanelJoints.cpp
@@ -907,9 +907,7 @@ void ndAssetEditor::EditDoubleHingeJoint()
 
 void ndAssetEditor::EditWheelJoint()
 {
-	//EditLoopJointGlobalMatrix();
 	EditJointGlobalMatrix();
-
 	ndMeshJointWheel* const joint = (ndMeshJointWheel*)*m_currentSelection->GetJoint();
 
 	ImGui::SeparatorText("actuator params");
@@ -950,11 +948,11 @@ void ndAssetEditor::EditWheelJoint()
 		m_undoRedo.Push(ndSharedPtr<ndUndoRedoCommand>(new ndUndoRedoStructuralJoint(this, *m_currentSelection)));
 	}
 
-	value = ndReal(joint->m_desc->m_steeringAngle);
+	value = ndReal(joint->m_desc->m_steeringAngle * ndRadToDegree);
 	if (ImGui::InputFloat("steering angle", &value, 0.0, 0.0, "%.3f", ImGuiInputTextFlags_EnterReturnsTrue))
 	{
 		m_undoRedo.Push(ndSharedPtr<ndUndoRedoCommand>(new ndUndoRedoStructuralJoint(this, *m_currentSelection)));
-		joint->m_desc->m_steeringAngle = ndClamp(value, ndReal(0.0f), ndReal(45.0f));
+		joint->m_desc->m_steeringAngle = ndDegreeToRad * ndClamp(value, ndReal(0.0f), ndReal(45.0f));
 		m_undoRedo.Push(ndSharedPtr<ndUndoRedoCommand>(new ndUndoRedoStructuralJoint(this, *m_currentSelection)));
 	}
 
