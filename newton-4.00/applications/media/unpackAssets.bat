4b5ae1d6504f0cc83dbcc1ba9ec4075495f21a75
NewtonDynamics <jerezjulio0@gmail.com>
2026-07-07 13:27:42 -0700

    using compress file on github. (wip)

--- newton-4.00/applications/media/unpackAssets.bat
+++ newton-4.00/applications/media/unpackAssets.bat
@@ -1,18 +1,11 @@
 @echo off
 setlocal
 
-rem Define the starting directory. Use "." for the current directory.
-set "src_dir=."
-set "dst_dir=xxx/"
+rem define the starting directory. Use "." for the current directory.
+rem set "src_dir=."
 
-echo xxxxxxxxxxxxx
-echo "%dst_dir%"
-echo zzzzzzzzzzzzz
-
-for /R "%src_dir%" %%f in (*.zip) do (
-    echo "processing %%f"
-    rem fbxToMesh.exe %%f
-    tar -xf %%f -C "%dst_dir%"
+for %%f in ("*.zip") do (
+    echo "unpacking: %%~nxf"
+    tar -xf %%~nxf
 )
-pause
 endlocal
