9156985980ff4cc17a0d182ca0da3bc6c4b3aefb
JulioJerez <jerezjulio0@gmail.com>
2026-08-30 10:11:05 -0700

    Update ndUnicycleTrainner_ppo.cpp

--- newton-4.00/applications/ndSandbox/reiforcementLearning/ndUnicycleTrainner_ppo.cpp
+++ newton-4.00/applications/ndSandbox/reiforcementLearning/ndUnicycleTrainner_ppo.cpp
@@ -124,7 +124,7 @@ namespace ndUnicycleTrainer_ppo
 			hyperParameters.m_numberOfHiddenLayers = 3;
 			hyperParameters.m_maxTrajectorySteps = 2048;
 			hyperParameters.m_batchTrajectoryCount = 1000;
-			hyperParameters.m_discountRewardFactor = 0.99f;
+			hyperParameters.m_discountRewardFactor = 0.995f;
 			hyperParameters.m_hiddenLayersNumberOfNeurons = 128;
 			hyperParameters.m_numberOfActions = m_actionsSize;
 			hyperParameters.m_numberOfObservations = m_observationsSize;
