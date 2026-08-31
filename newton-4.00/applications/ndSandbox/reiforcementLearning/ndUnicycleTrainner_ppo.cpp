8271f7ddef4705094418e627163b2bb48c0920d2
NewtonDynamics <jerezjulio0@gmail.com>
2026-08-31 15:39:40 -0700

    Update ndUnicycleTrainner_ppo.cpp

--- newton-4.00/applications/ndSandbox/reiforcementLearning/ndUnicycleTrainner_ppo.cpp
+++ newton-4.00/applications/ndSandbox/reiforcementLearning/ndUnicycleTrainner_ppo.cpp
@@ -123,9 +123,9 @@ namespace ndUnicycleTrainer_ppo
 			hyperParameters.m_useGpuBackend = false;
 			hyperParameters.m_numberOfHiddenLayers = 3;
 			hyperParameters.m_maxTrajectorySteps = 2048;
-			hyperParameters.m_batchTrajectoryCount = 500;
+			hyperParameters.m_batchTrajectoryCount = 1000;
 			hyperParameters.m_discountRewardFactor = 0.995f;
-			hyperParameters.m_divergenceStopThreshold = 0.1f;
+			//hyperParameters.m_divergenceStopThreshold = 0.1f;
 			hyperParameters.m_hiddenLayersNumberOfNeurons = 128;
 			hyperParameters.m_numberOfActions = m_actionsSize;
 			hyperParameters.m_numberOfObservations = m_observationsSize;
