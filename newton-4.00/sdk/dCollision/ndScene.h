a8ed5ae034cc5a7b30a3f1c9866b4bc666d2394b
NewtonDynamics <jerezjulio0@gmail.com>
2026-09-20 17:06:44 -0700

    Update ndScene.h

--- newton-4.00/sdk/dCollision/ndScene.h
+++ newton-4.00/sdk/dCollision/ndScene.h
@@ -39,7 +39,7 @@ class ndConvexCastNotify;
 class ndBodiesInAabbNotify;
 class ndJointBilateralConstraint;
 
-D_MSV_NEWTON_CLASS_ALIGN_32
+
 class ndSceneTreeNotiFy : public ndClassAlloc
 {
 	public:
@@ -52,7 +52,7 @@ class ndSceneTreeNotiFy : public ndClassAlloc
 	}
 
 	virtual void OnDebugNode(const ndBvhNode* const node) = 0;
-} D_GCC_NEWTON_CLASS_ALIGN_32;
+};
 
 D_MSV_NEWTON_CLASS_ALIGN_32
 class ndScene : public ndThreadPool
